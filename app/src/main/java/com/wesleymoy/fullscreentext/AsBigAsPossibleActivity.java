7ae3a41742177852dcfd4afe4e3deda3f0b88086
Wesley Moy <wesley.moy@gmail.com>
2015-12-20 15:09:51 -0800

    Minimally working custom view

---
+++ app/src/main/java/com/wesleymoy/fullscreentext/AsBigAsPossibleActivity.java
@@ -0,0 +1,13 @@
+package com.wesleymoy.fullscreentext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsBigAsPossibleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_as_big_as_possible_text_view);
+    }
+}
---
+++ app/src/main/java/com/wesleymoy/fullscreentext/AsBigAsPossibleTextView.java
@@ -0,0 +1,236 @@
+package com.wesleymoy.fullscreentext;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class AsBigAsPossibleTextView extends View {
+    private static final String TAG = "AsBigAsPossibleTextView";
+    private String mExampleString; // TODO: use a default from R.string...
+    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
+    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
+    private Drawable mExampleDrawable;
+
+    private TextPaint mTextPaint;
+    private float mTextWidth;
+    private float mTextHeight;
+    public static final Pattern LAST_WHITESPACE = Pattern.compile("\\s\\S*$");
+
+    public AsBigAsPossibleTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public AsBigAsPossibleTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public AsBigAsPossibleTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.AsBigAsPossibleTextView, defStyle, 0);
+
+        mExampleString = a.getString(
+                R.styleable.AsBigAsPossibleTextView_exampleString);
+        mExampleColor = a.getColor(
+                R.styleable.AsBigAsPossibleTextView_exampleColor,
+                mExampleColor);
+        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
+        // values that should fall on pixel boundaries.
+        mExampleDimension = a.getDimension(
+                R.styleable.AsBigAsPossibleTextView_exampleDimension,
+                mExampleDimension);
+
+        if (a.hasValue(R.styleable.AsBigAsPossibleTextView_exampleDrawable)) {
+            mExampleDrawable = a.getDrawable(
+                    R.styleable.AsBigAsPossibleTextView_exampleDrawable);
+            mExampleDrawable.setCallback(this);
+        }
+
+        a.recycle();
+
+        // Set up a default TextPaint object
+        mTextPaint = new TextPaint();
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+
+        // Update TextPaint and text measurements from attributes
+        invalidateTextPaintAndMeasurements();
+    }
+
+    private void invalidateTextPaintAndMeasurements() {
+        mTextPaint.setTextSize(mExampleDimension);
+        mTextPaint.setColor(mExampleColor);
+        mTextPaint.setTextSize(2000);
+        mTextWidth = mTextPaint.measureText(mExampleString);
+
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        mTextHeight = fontMetrics.bottom;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        mExampleString = mExampleString.trim();
+        List<String> lines = new ArrayList<>();
+        boolean tooBig = true;
+        while (tooBig) {
+            int start = 0;
+            lines.clear();
+            tooBig = false;
+            mTextPaint.setTextSize(mTextPaint.getTextSize() - 10);
+            Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+            Log.d(TAG, "text size: " + mTextPaint.getTextSize());
+            while (start < mExampleString.length() - 1 && !tooBig) {
+                int i = mTextPaint.breakText(mExampleString, start, mExampleString.length(), true, contentWidth, null);
+                Log.d(TAG, "i: " + i);
+                if (i == mExampleString.length() - start) {
+                    lines.add(mExampleString.substring(start, start + i));
+                    start += i;
+                    continue;
+                }
+                Matcher matcher = LAST_WHITESPACE.matcher(mExampleString).region(start, start + i);
+                if (!matcher.find()) {
+                    tooBig = true;
+                } else {
+                    int whitespace = matcher.start();
+                    lines.add(mExampleString.substring(start, whitespace).trim());
+                    start = whitespace + 1;
+                }
+            }
+
+            float size = (-fontMetrics.ascent + fontMetrics.descent) * lines.size() + fontMetrics.leading * (lines.size() - 1);
+            if (size > contentHeight) {
+                tooBig = true;
+            }
+        }
+
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        float size = (-fontMetrics.ascent + fontMetrics.descent) * lines.size() + fontMetrics.leading * (lines.size() - 1);
+        float top = paddingTop + (contentHeight - size) / 2;
+        for (int i = 0; i < lines.size(); i++) {
+            canvas.drawText(lines.get(i),
+                    paddingLeft + contentWidth / 2,
+                    top + (-fontMetrics.ascent + fontMetrics.descent + fontMetrics.leading) * i -fontMetrics.ascent,
+                    mTextPaint);
+        }
+
+        // Draw the example drawable on top of the text.
+        if (mExampleDrawable != null) {
+            mExampleDrawable.setBounds(paddingLeft, paddingTop,
+                    paddingLeft + contentWidth, paddingTop + contentHeight);
+            mExampleDrawable.draw(canvas);
+        }
+    }
+
+    /**
+     * Gets the example string attribute value.
+     *
+     * @return The example string attribute value.
+     */
+    public String getExampleString() {
+        return mExampleString;
+    }
+
+    /**
+     * Sets the view's example string attribute value. In the example view, this string
+     * is the text to draw.
+     *
+     * @param exampleString The example string attribute value to use.
+     */
+    public void setExampleString(String exampleString) {
+        mExampleString = exampleString;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example color attribute value.
+     *
+     * @return The example color attribute value.
+     */
+    public int getExampleColor() {
+        return mExampleColor;
+    }
+
+    /**
+     * Sets the view's example color attribute value. In the example view, this color
+     * is the font color.
+     *
+     * @param exampleColor The example color attribute value to use.
+     */
+    public void setExampleColor(int exampleColor) {
+        mExampleColor = exampleColor;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example dimension attribute value.
+     *
+     * @return The example dimension attribute value.
+     */
+    public float getExampleDimension() {
+        return mExampleDimension;
+    }
+
+    /**
+     * Sets the view's example dimension attribute value. In the example view, this dimension
+     * is the font size.
+     *
+     * @param exampleDimension The example dimension attribute value to use.
+     */
+    public void setExampleDimension(float exampleDimension) {
+        mExampleDimension = exampleDimension;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example drawable attribute value.
+     *
+     * @return The example drawable attribute value.
+     */
+    public Drawable getExampleDrawable() {
+        return mExampleDrawable;
+    }
+
+    /**
+     * Sets the view's example drawable attribute value. In the example view, this drawable is
+     * drawn above the text.
+     *
+     * @param exampleDrawable The example drawable attribute value to use.
+     */
+    public void setExampleDrawable(Drawable exampleDrawable) {
+        mExampleDrawable = exampleDrawable;
+    }
+}
